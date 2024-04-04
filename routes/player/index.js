1d9b2f9270f6bcf60bc61c4414de8a303b61be0f
SoleneHirles <86622879+SoleneHirles@users.noreply.github.com>
2024-04-04 17:25:54 +0200

    Documentation Open API for Dungeons and Players (Request level1) (#23)
    * Pagination Players
    
    * pagination Dungeons
    
    * update pagination
    
    * Init Documentation Open API
    
    * OpenAPI update params get /api/players
    
    * OpenAPI- Finish post api/players/
    
    * OpenAPI all players request level1
    
    * OpenAPI- Request dungeons lvl1 + correction request players
    
    ---------
    
    Co-authored-by: SHirles <solene.hirles@mindpulse.net>

--- routes/player/index.js
+++ routes/player/index.js
@@ -4,6 +4,20 @@ const {pagesize}=require("../../queryConfig");
 
 const router = Router();
 
+//Pagination
+router.get("/:page",async (req,res)=>{
+    var {page} =req.params;
+    if(page===undefined || page<=0){
+        page=1;
+    }
+    const players=await client.player.findMany({
+        skip: (page-1)*pagesize,
+        take: pagesize,
+    })
+
+    res.status(200).json(players);
+});
+
 // Récupérer tous les livres
 router.get("/", async (req, res) => {
     var {page, size} = req.query;
